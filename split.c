e07d5701ba8da466e1ed45c02cd7a0048945af21
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-09-24 14:09:36 +0200

    doing the split and add ft_isspace

--- split.c
+++ split.c
@@ -6,40 +6,68 @@
 /*   By: meghribe <meghribe@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 00:09:59 by meghribe          #+#    #+#             */
-/*   Updated: 2025/09/24 00:18:37 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/09/24 14:09:13 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "push_swap.h"
 
 /* c is the delimiter */
-size_t	count_delimiters(const char *s, char delimiter)
+void	count_words(const char *s, char delimiter, size_t number_words)
 {
 	size_t	i;
-	size_t	delimiter_counter;
+	size_t	word_counter;
+	int	in_word;
 
 	i = 0;
-	delimiter_counter = 0;
-	while (s[i])
+	number_words = 0;
+	in_word = 0;
+	while (s[i] != '\0')
 	{
-		if ((char)s[i] == delimiter)
-			delimiter_counter++;
+		if (s[i] == delimiter)
+		{
+			in_word = 0;
+			i++;
+			continue;
+		}
+		if (in_word == 0)
+		{
+			number_words++;
+			in_word = 1;
+		}
 		i++;
 	}
-	return (delimiter_counter);
+}
+
+char	**copy_each_word(const char *s, char c, size_t quantity, char **array)
+{
+
+}
+
+void	free_split(char **s, size_t total_words)
+{
 
 }
 
 char	**ft_split(const char *s, char c)
 {
-	(void)s,(void)c;
+	size_t	total_words;
+	char**	splitted_words;
+
+	count_words(s, c, total_words);
+	splitted_words = (char **)malloc(sizeof(char *) * (total_words + 1));
+	if (splitted_words == NULL)
+		return (NULL);
+	copy_each_word(s, c, total_words, splitted_words);
 	return (NULL);
 }
 
 int	main(void)
 {
-	(void)ft_split("A a a a ", 'a');
-	printf("%li", count_delimiters("buenos dias marta  ", ' '));
+	ft_split("A a a a ", 'a');
+//printf("%li", count_delimiters("buenos dias marta  ", ' '));
 	return (0);
 }
--- utils.c
+++ utils.c
@@ -6,12 +6,17 @@
 /*   By: meghribe <meghribe@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 23:41:09 by meghribe          #+#    #+#             */
-/*   Updated: 2025/09/23 23:58:30 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/09/24 13:50:18 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stddef.h>
+
+int	ft_isspace(int c)
+{
+	return (c == 32 || (c >= 9 && c <= 13));
+}
 
 size_t	ft_strlen(const char *s)
 {
